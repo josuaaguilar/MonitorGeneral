1e0477a1f8c24c4abe8834fc2ebb3af134060c23
Josua <aguilar.josua13011069@gmail.com>
2021-07-24 11:58:53 -0500

    Se limpian comentarios

--- src/Component/ListResult.js
+++ src/Component/ListResult.js
@@ -1,41 +1,20 @@
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 function ListResult (props){
     let url = props.url; 
     const [Result, setResult] = useState([])
     const [isUpdate,setUpdate] = useState(true)
     let unique_unitArr = []
-    /*
-    res -> {
-        "id": 79, 
-        "name": "Jaguar Warrior", 
-        "description": "Aztec unique unit. Attack bonus vs. other infantry", 
-        "expansion": "The Conquerors", 
-        "age": "Castle", 
-        "created_in": "https://age-of-empires-2-api.herokuapp.com/api/v1/structure/castle", 
-        "cost": {
-            "Food": 60, 
-            "Gold": 30
-    }*/
-
-
     useEffect(async () => {
-        console.log(Result) // primera pasada : verctor vacio []
         if(isUpdate){
-            console.log("is update true")
             url.map(async item => {
-                //console.log(item) -> la url por cada posicion del vector en el objeto : {url : ["",""...]}
                 const data = await fetch(item)
                 const res = await data.json()
                 unique_unitArr.push(res)
                 setResult(unique_unitArr)    
-                //console.log(Result)  -> Vacio 
             })
             setUpdate(false)
-        } else {
-            console.log("Is update false")
-        }
-        console.log(Result) // muestra el objeto { id:0, name:"samu"....}
-        })
+        } 
+    })
     return(        
             <ul>
             {
